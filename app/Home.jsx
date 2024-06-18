1a44efd89368580a16cda3462528fd777910c9ae
Josenov <josenovillo88@gmail.com>
2024-06-18 17:59:56 -0300

    project structure done

--- app/Home.jsx
+++ app/Home.jsx
@@ -6,10 +6,10 @@ import { Link } from 'expo-router';
 
 export default function App() {
     return (
-        <View className='flex-1 items-center justify-center bg-yellow-400'>
-            <Text className='text-2xl font-bold' >Hola Mundo de React Native!</Text>
+        <View className='flex-1 items-center justify-center bg-[#fafafa]'>
+            <Text className='text-2xl font-pblack' >Hola Mundo!</Text>
             <StatusBar style="auto" />
-            <Link className='text-xl text-blue-400 font-semibold' href= '/Profile' >Go To Profile</Link>
+            <Link className='text-xl text-blue-400 font-semibold' href= '/home' >Go To Home</Link>
         </View>
     );
 }
