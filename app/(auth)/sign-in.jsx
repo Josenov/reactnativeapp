0441ca1271a41e2e94dcbd3934526807ac707b7f
Josenov <josenovillo88@gmail.com>
2024-06-27 15:36:18 -0300

    sign up and sign in screen working, global context in progress

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,24 +1,44 @@
-import { View, ScrollView, Image, Text } from 'react-native'
+import { View, ScrollView, Image, Text, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import {images} from '../../constants'
+import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
+import { signIn } from '../../lib/appwrite'
 
 
 
 const SignIn = () => {
 
   const [form, setForm] = useState({
-    email:'',
-    password:''
+    email: '',
+    password: ''
   });
 
   const [isSubmitting, setIsSubmitting] = useState(false);
-  
 
-  const submit = () => {
+
+  const submit = async () => {
+
+    if ( !form.email || !form.password) {
+      Alert.alert('Error', 'Please fill all the fields')
+    }
+
+    setIsSubmitting(true)
+    try {
+       await signIn(form.email, form.password)
+
+      //set it to global state
+
+      router.replace('/home')
+
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setIsSubmitting(false)
+    }
+
 
   }
 
@@ -26,44 +46,44 @@ const SignIn = () => {
     <SafeAreaView className='bg-primary h-full'>
       <ScrollView>
         <View className='w-full justify-center min-h-[90vh] px-4 my-6'>
-            <Image
-            
+          <Image
+
             source={images.logo}
             resizeMode='contain'
             className='w-[115px] h-[35px]'
-            
-            />
-            <Text className='text-2xl text-semibold mt-10 font-psemibold  text-white'>Log In To Aora</Text>
-            <FormField
-              title='Email'
-              value={form.email}
-              handleChangeText = {(e) => setForm({...form, email:e})}
-              otherStyles='mt-7'
-              keyboarType='email-adress'
-            
-            />
-            <FormField
-              title='Password'
-              value={form.password}
-              handleChangeText = {(e) => setForm({...form, password:e})}
-              otherStyles='mt-7'
-              
-            
-            />
-            <CustomButton
-              title='Sign In'
-              handlePress={submit}
-              containerStyles='mt-7'
-              isLoading={isSubmitting}
-            
-            />
-            <View className='justify-center items-center pt-5 flex-row gap-2'>
-              <Text className='text-lg text-gray-200 font-pregular'>
-                Don't have an account?
-              </Text>
-              <Link className='text-lg font-semibold text-secondary' href={'/sign-up'}>Sign Up</Link>
 
-            </View>
+          />
+          <Text className='text-2xl text-semibold mt-10 font-psemibold  text-white'>Log In To Aora</Text>
+          <FormField
+            title='Email'
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles='mt-7'
+            keyboarType='email-adress'
+
+          />
+          <FormField
+            title='Password'
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles='mt-7'
+
+
+          />
+          <CustomButton
+            title='Sign In'
+            handlePress={submit}
+            containerStyles='mt-7'
+            isLoading={isSubmitting}
+
+          />
+          <View className='justify-center items-center pt-5 flex-row gap-2'>
+            <Text className='text-lg text-gray-200 font-pregular'>
+              Don't have an account?
+            </Text>
+            <Link className='text-lg font-semibold text-secondary' href={'/sign-up'}>Sign Up</Link>
+
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
